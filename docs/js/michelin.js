fc487b2554d26cbbb0a6a8ae2f37d6903f49225a
miguel ramos <miguel-ramos@hotmail.fr>
2018-02-07 04:06:00 +0100

    request retry implemented, clean code michelin.js

--- docs/js/michelin.js
+++ docs/js/michelin.js
@@ -1,14 +1,90 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
+//var request = require('request');
+var request = require('requestretry'); //When the connection fails with one of ETIMEDOUT the request will automatically be re-attempted as these are often recoverable errors and will go away on retry
+var cheerio = require('cheerio');      //Fast, flexible & lean implementation of core jQuery designed specifically for the server
+var fs = require('fs');                //work with the file system
 
 var json = { "restaurants": [] };
 var starter_url = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin";
+var count_resto_urls = 0;           //Limit for async function
+var urls_restaurant=[];             //Contains the ursl of all the stared restaurants
+var json_name="restaurants.json";
+var i = 0;
+
+//Determine the number of pages to scrape based on the first page url
+function getNbPagestoScrape(url,done){
+	var urls_pages=[];
+  	request(url, function(err,resp,body){ 
+  		 if (err) console.log("error at url : " + url);
+  		 else{
+  		 	 var $=cheerio.load(body);
+    		 $('.mr-pager-link').each(function(){
+      		 	 var url_temp=$(this).attr('attr-page-number');
+      	  		 urls_pages.push(Number(url_temp));            //Page numbers are added to the array urls_pages
+    		 });
+  		 	 var nbPages=Math.max(...urls_pages); //the maximum number of page dertermine number of pages to scrape
+  			 //next();
+  			 done(null,nbPages);                   //Once the process is done, we catch the number of pages to scrape
+  		 } 
+ 	 });
+}
+
+//Determine the number of restaurants to scrape based on the results of the starter_url (615 - 05/02/2018)
+function getNbRestaurants(url,done){
+  	request(url, function(err,resp,body){ 
+  		 if (err) console.log("error at url : " + url);
+  		 else{
+  		 	 var $=cheerio.load(body);
+    		 var nbRestaurants = $('#invoke-search').val();
+    		 nbRestaurants = nbRestaurants.replace(/\D/g,'');
+  			 done(null,nbRestaurants);                   //Once the process is done, we catch the number of restaurants
+  		 } 
+ 	 });
+}
+
+//Scrape all the restaurants urls on every page url (as parameter)
+function scrapeUrls(url,done){
+	 //var urls_restaurant=[];
+  	 request(url, function(err,resp,body){  
+  	 	 if (err) console.log("error at url : " + url);
+  	 	 else{
+  	 	 	 var $=cheerio.load(body);
+    		 $('.poi-card-link').each(function(){
+      		 	 var url_temp = "https://restaurant.michelin.fr";
+      			  url_temp+=$(this).attr('href');
+      		 	 urls_restaurant.push(url_temp);              //Every restaurant name is added to the array of restaurants
+      		 });
+      		 urls_Limit(urls_restaurant);
+  			 done(null,urls_restaurant);     //Once the process is done, we catch the restaurant urls
+  	 	 }
+  		
+  	});
+}
+
+//Function which helps to wait for the async function scrapeUrls to finish
+function urls_Limit(urls_restaurant){
+	count_resto_urls=urls_restaurant.length;
+	//console.log(count_resto_urls);
+}
+
+/*function count_i() //Debug
+{
+	i++;
+	console.log(i);
+}*/
 
 //Scrape information on the restaurant url
-function getRestaurant(url,callback){
-	request(url, function(err,resp,html){
-		if (!err) {
+function getRestaurant(url,done){
+	request({
+  				 url: url,
+  			 	 json: true, // The below parameters are specific to request-retry
+  				 maxAttempts: 3,   //  try 3 times
+  	 		  	 retryDelay: 10,  //  wait for 0.005s before trying again
+  		 		 retryStrategy: request.RetryStrategies.HTTPOrNetworkError // (default) retry on 5xx or network errors
+			},
+	function(err,resp,html){
+		if (err) console.log("error at url : " + url);
+		else{
+			count_i();
 			const $=cheerio.load(html);
 			var name = $('h1').first().text();
 			var address = $('.thoroughfare').first().text();
@@ -22,61 +98,43 @@ function getRestaurant(url,callback){
 				 "city": city,
 				 "chef": chef
 			};
-  			callback(restaurant); //Once the process is done, we catch the restaurant info
+
+			make_Json(restaurant);
+  			done(null,restaurant); //Once the process is done, we catch the restaurant info
 		}
 	});
 }
 
-
-//Scrape all the restaurants urls on every page url (as parameter)
-function scrapeUrls(url,callback){
-	 var urls_restaurant=[];
-  	 request(url, function(err,resp,body){  
-  		var $=cheerio.load(body);
-    	$('.poi-card-link').each(function(){
-      		 var url_temp = "https://restaurant.michelin.fr";
-      		 url_temp+=$(this).attr('href');
-      		 urls_restaurant.push(url_temp);              //Every restaurant name is added to the array of restaurants
-      	});
-  		callback(urls_restaurant);     //Once the process is done, we catch the restaurant urls
-  	});
-}
-
-//Determine the number of pages to scrape based on the first page url
-function nbPagestoScrape(url,callback){
-	var urls_pages=[];
-  	request(url, function(err,resp,body){  
-    	var $=cheerio.load(body);
-    	$('.mr-pager-link').each(function(){
-      		var url_temp=$(this).attr('attr-page-number');
-      		urls_pages.push(Number(url_temp));            //Page numbers are added to the array urls_pages
-    	});
-  		var nbPages=Math.max(...urls_pages); //the maximum number of page dertermine number of pages to scrape
-  		callback(nbPages);                   //Once the process is done, we catch the number of pages to scrape
- 	 });
+// Function add restaurant to json array and make a file from the json array
+function make_Json(restaurant)
+{
+	json.restaurants.push(restaurant);
+	fs.writeFile(json_name,JSON.stringify(json),'utf-8',
+		function(err){  //store the data 
+          	 if(err) console.log("error with restaurant " + restaurant);
+             else return console.log(json.restaurants.length+" restaurants added to json file");
+         });
 }
 
 
-//Scrape restaurants using callbacks functions to handle data
-function Scrape(url){
-  	nbPagestoScrape(url,function(nbPages){
-    	for(var i=1; i<=nbPages; i++){
-     	 	 var nextUrl = url+"/page-"+String(i);
-      	 	 scrapeUrls(nextUrl,function(urls_restaurant){
-         	 	 urls_restaurant.forEach(function (ur){
-         	 	 	 getRestaurant(ur,function(restaurant){
-             			 json.restaurants.push(restaurant);
-             				 fs.writeFile('restaurants.json',JSON.stringify(json),'utf-8',function(err){  //store the data 
-             	 				 if(err) return console.log(err);
-             			 		 else return console.log("json processing...");
-            		 		});
-             		 	 console.log(json);
-          	 	 	 }); 
-         	 	 });
-          	 });
-    	 }
-  	});
+//Main function
+function scrapeAllRestaurants(url){
+	 getNbPagestoScrape(url,function(err,nbPages){
+	 	getNbRestaurants(starter_url,function (err,nbRestaurants){
+	 	 	for(var i=1; i<=nbPages; i++){ //starts at page-1
+	 	 		var newPageUrl=url+"/page-"+String(i);
+	 	 	 	scrapeUrls(newPageUrl,function(err,urls_restaurant){
+	 	 	 		 if(count_resto_urls==nbRestaurants){                   //help the async function scrapeUrls to finish, otherwise we don't have all the urls to continue the getrestaurant()
+	 	 	 	 		 urls_restaurant.forEach(function(url_resto){
+	 	 	 	 	 		 getRestaurant(url_resto,function(err,restaurant){
+	 	 	 	 	 		 	//console.log(restaurant);
+	 	 	 	 	 	 	})
+	 	 	 	 	 	})
+	 	 	 		 }
+	 	 	 	})
+	   	 	 }
+	   	 })
+	 })
 }
 
-Scrape(starter_url);
-
+scrapeAllRestaurants(starter_url);
